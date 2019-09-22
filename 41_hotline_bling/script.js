b6f1023a718b51440d1525876cbbc7836d8bfca7
ThiagoRAD <thauroo@gmail.com>
2019-09-22 13:07:53 -0300

    update

---
+++ 41_hotline_bling/script.js
@@ -0,0 +1,5 @@
+const drakePicTwo = document.getElementById("drake-pic-2");
+const heading = document.getElementById('heading-2')
+
+drakePicTwo.src = "https://i.imgur.com/RGbh6zY.png"
+heading.textContent = "Learn By Doing"
