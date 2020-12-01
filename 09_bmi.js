b64ac7c7bbfe9cc7c0bedebc486de762fd9dec04
ThiagoRAD <thauroo@gmail.com>
2020-12-01 13:53:53 -0300

    bmi

---
+++ 09_bmi.js
@@ -0,0 +1,8 @@
+const height = 1.80
+const weight = 75
+
+const bmi = weight / (height * height)
+
+console.log("Height (m): " + height)
+console.log("Weight (kg): " + weight)
+console.log("BMI: " + bmi)
