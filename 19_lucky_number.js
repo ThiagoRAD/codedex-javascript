03ee05b49d689887e4f48ff1d46520c6fc78808a
ThiagoRAD <thauroo@gmail.com>
2023-02-10 17:00:14 -0300

    19

--- 19_lucky_number.js
+++ 19_lucky_number.js
@@ -1,4 +1,10 @@
 const luckyNumber = 7
-let guess = Math.floor(Math.random() * 10) + 1;
 
-console.log("Your guess: " + guess)
+while (true) { {
+  let guess = Math.floor(Math.random() * 10) + 1;
+  console.log("Your guess: " + guess)
+  if (guess === luckyNumber) {
+    console.log("Congratulations! You guessed the lucky number!");
+    break;
+  }
+}
