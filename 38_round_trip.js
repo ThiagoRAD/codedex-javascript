cb3f0cf5561e1db4b4d3bbec51f39eb8b199dff3
ThiagoRAD <thauroo@gmail.com>
2023-09-29 12:57:45 -0300

    update

---
+++ 38_round_trip.js
@@ -0,0 +1,13 @@
+const departTripTicket = {
+  from: "New York",
+  to: "Los Angeles",
+  date: "2024-07-01",
+}
+
+const returnTripTicket = {
+  from: "Los Angeles",
+  to: "New York",
+  date: "2024-07-15",
+}
+console.log("Departure Trip:", departTripTicket)
+console.log("Return Trip:", returnTripTicket)
