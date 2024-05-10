e8397355374f32579aa4daedfe290052f9d60edd
ThiagoRAD <thauroo@gmail.com>
2024-05-10 13:40:18 -0300

    five boroughs

--- 32_five_boroughs.js
+++
@@ -1,9 +0,0 @@
-let myDestinations = [
-  "🗽 Manhattan - Washington Square Park",
-  "🏟️ The Bronx - Yankee Stadium",
-  "🎡 Brooklyn - Coney Island",
-  "🧧 Queens - Flushing",
-  "🌉 Staten Island - Historic Richmond Town"
-];
-
-console.log(myDestinations);
