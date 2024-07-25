80ebc3896461fee9831056df7d8b78366b834b25
ThiagoRAD <thauroo@gmail.com>
2024-07-25 13:51:06 -0300

    data types

---
+++ 07_data_types.js
@@ -0,0 +1,9 @@
+const companyName = "DevX"
+const foundingYear = 2025
+const isActive = true
+let fundingAmount
+
+console.log("Company Name: " + companyName)
+console.log("Founding Year: " + foundingYear)
+console.log("Is Active: " + isActive)
+console.log("Funding Amount: " + fundingAmount)
