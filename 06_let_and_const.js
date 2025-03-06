778c4871869fcbe349748bc0619c53c26e3d9d6b
ThiagoRAD <thauroo@gmail.com>
2025-03-06 13:49:06 -0300

    let and const

--- 06_let_and_const.js
+++ 06_let_and_const.js
@@ -2,3 +2,10 @@ const name = "Black"
 const color = "black"
 let location = "the internet"
 let mood = "curious"
+
+location = "the world wide web"
+
+console.log("My name is " + name + ".")
+console.log("My favorite color is " + color + ".")
+console.log("I am currently browsing " + location + ".")
+console.log("I feel " + mood + " today.")
